e3f04ef8d330e4d45be8d3e147a36512d0aa1571
katherine-07 <luisa_gilig@dlsu.edu.ph>
2017-08-10 13:28:42 +0800

    Email Utility + static user status

--- SECURDE_Library/src/models/User.java
+++ SECURDE_Library/src/models/User.java
@@ -21,12 +21,14 @@ public class User implements Serializable{
 	public final static String COLUMN_STATUS = "status";
 	public final static String COLUMN_ATTEMPT = "attempt";
 	
-
 	public final static int STUDENT = 0;
 	public final static int FACULTY = 1;
 	public final static int MANAGER = 2;
 	public final static int STAFF = 3;
 	public final static int ADMINISTRATOR = 4;
+	
+	public final static int STATUS_UNLOCKED = 0;
+	public final static int STATUS_LOCKED = 1;
 	
 	private String firstName, middleName, lastName;
 	private int idUser;
